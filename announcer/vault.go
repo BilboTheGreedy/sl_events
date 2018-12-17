12e0f1983726ca9e01e5264a8b0d76d0d1283ad7
Daniel Rapp <daniel.rapp@live.se>
2018-12-17 15:21:42 +0100

    cleaned up code

--- announcer/vault.go
+++ announcer/vault.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/hashicorp/vault/api"
 )
 
-var VClient *api.Client  // global variable
-var slack_channel string // global variable
-var slack_token string   // global variable
+var VClient *api.Client // global variable
 
 func InitVault(server string, token string) error {
 	conf := &api.Config{
@@ -25,36 +22,4 @@ func InitVault(server string, token string) error {
 
 	VClient.SetToken(token)
 	return nil
-}
-
-func readVault(server string, keyName string, token string) {
-	err := InitVault(server, token)
-	if err != nil {
-		log.Println(err)
-
-	} else {
-		printMsg("Connecting to Vault")
-	}
-	secretValues, err := VClient.Logical().Read(keyName)
-	if err != nil {
-		log.Println(err)
-
-	}
-	//TODO: make sure to select correct kv pair if multiple in data
-	for k, v := range secretValues.Data {
-		secretSwitch(k, fmt.Sprintf("%v", v), keyName)
-	}
-
-}
-
-func secretSwitch(k string, v string, keyName string) {
-	switch k {
-	case "slack_token":
-		slack_token = v
-	case "slack_channel":
-		slack_channel = v
-	default:
-
-	}
-
 }
